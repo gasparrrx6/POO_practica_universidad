e3111b017d8902ca9070b3df4c26cc9b8edd8741
Gaspar Pelayo <gasparpelayo426@gmail.com>
2022-09-23 22:12:00 -0300

    U3-Ej2 terminado

--- U3-Relaciones_entre_clases/U3-Ej2_clase.cpp
+++ U3-Relaciones_entre_clases/U3-Ej2_clase.cpp
@@ -12,6 +12,7 @@ private:
   string estadoCivil;
 
 public:
+  Persona(){};
   Persona(string apellido, string nombre, int dni, int fechaNac,
           string estadoCivil);
 
@@ -30,6 +31,7 @@ private:
   int cantMaterias;
 
 public:
+  Alumno(){};
   Alumno(float promedio, int cantMaterias, string apellido, string nombre,
          int dni, int fechaNac, string estadoCivil);
   float meritoAcademico();
@@ -58,8 +60,64 @@ public:
   Alumno mejorPromedio();
 };
 
-int main() { return 0; }
+int main() {
+  string apellido, nombre, estadoCivil, nombreCurso;
+  int dni, fechaNac, cantMaterias, fechaIngreso;
+  float promedio = 0;
+  Alumno a;
 
+  cout << "Ingrese nombre del curso\n";
+  cin >> nombreCurso;
+
+  cout << "\nDatos del docente\n";
+  cout << "Fecha de ingreso formato ddmmaaaa\n";
+  cin >> fechaIngreso;
+  cout << "Apellido\n";
+  cin >> apellido;
+  cout << "Nombre\n";
+  cin >> nombre;
+  cout << "DNI\n";
+  cin >> dni;
+  cout << "Fecha de nacimiento formato ddmmaaaa\n";
+  cin >> fechaNac;
+  cout << "Estado civil\n";
+  cin >> estadoCivil;
+
+  Docente _docente(fechaIngreso, apellido, nombre, dni, fechaNac, estadoCivil);
+  Curso curso(nombreCurso, _docente);
+
+  cout << "\nDatos del Alumno\n";
+  cout << "Promedio\n";
+  cin >> promedio;
+  do {
+    cout << "Cantidad de materias\n";
+    cin >> cantMaterias;
+    cout << "Apellido\n";
+    cin >> apellido;
+    cout << "Nombre\n";
+    cin >> nombre;
+    cout << "DNI\n";
+    cin >> dni;
+    cout << "Fecha de nacimiento formato ddmmaaaa\n";
+    cin >> fechaNac;
+    cout << "Estado civil\n";
+    cin >> estadoCivil;
+    Alumno b(promedio, cantMaterias, apellido, nombre, dni, fechaNac,
+             estadoCivil);
+    curso.agregarAlumno(b);
+    cout << "\nDatos del Alumno (promedio -1 para terminar)\n";
+    cout << "Promedio\n";
+    cin >> promedio;
+  } while (promedio != -1);
+
+  a = (curso.mejorPromedio());
+  cout << "El alumno con mejor promedio es: " << a.getNombre() << " DNI "
+       << a.getDni() << " y promedio " << a.getPromedio();
+
+  return 0;
+}
+
+// Clase Persona
 Persona::Persona(string apellido, string nombre, int dni, int fechaNac,
                  string estadoCivil) {
   this->apellido = apellido;
@@ -70,6 +128,7 @@ Persona::Persona(string apellido, string nombre, int dni, int fechaNac,
 }
 int Persona::edad(int fechaActual) { return (fechaActual - fechaNac) / 10000; }
 
+// Clase Alumno
 Alumno::Alumno(float promedio, int cantMaterias, string apellido, string nombre,
                int dni, int fechaNac, string estadoCivil)
     : Persona(apellido, nombre, dni, fechaNac, estadoCivil) {
@@ -79,6 +138,7 @@ Alumno::Alumno(float promedio, int cantMaterias, string apellido, string nombre,
 float Alumno::meritoAcademico() { return cantMaterias * promedio; };
 float Alumno::getPromedio() { return promedio; }
 
+// Clase Docente
 Docente::Docente(int fechaIngreso, string apellido, string nombre, int dni,
                  int fechaNac, string estadoCivil)
     : Persona(apellido, nombre, dni, fechaNac, estadoCivil) {
@@ -88,6 +148,7 @@ int Docente::getAntiguedad(int fechaActual) {
   return (fechaActual - fechaIngreso) / 10000;
 };
 
+// Clase Curso
 Curso::Curso(string nombre, Docente newDocente)
     : d(newDocente) { // d es el atributo de la clase Curso
   this->nombre = nombre;
@@ -98,8 +159,9 @@ Alumno Curso::mejorPromedio() {
   float max = 0;
   for (int i = 0; i < listaAlumnos.size(); i++) {
     if (listaAlumnos[i].getPromedio() > max) {
-      a(listaAlumnos[i]);
+      a = (listaAlumnos[i]);
       max = listaAlumnos[i].getPromedio();
     }
   }
+  return a;
 }
