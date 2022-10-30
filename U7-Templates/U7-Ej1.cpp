e98a0103f9d86e4a177085505304dfb0dd6f6fd6
gasparrrx6 <gasparpelayo426@gmail.com>
2022-10-30 15:04:48 -0300

    Terminado U7-Ej1

--- U7-Templates/U7-Ej1.cpp
+++ U7-Templates/U7-Ej1.cpp
@@ -1,8 +1,8 @@
 /* Ejercicio 1
  *
  * Implemente una función templatizada llamada ​Mayor(...) que reciba dos
- * valores y devuelva el mayor. Compruebe el correcto funcionamiento de la
- * rutina probándola desde un programa cliente con valores de tipo ​int​,
+ * rta2es y devuelva el mayor. Compruebe el correcto funcionamiento de la
+ * rutina probándola desde un programa cliente con rta2es de tipo ​int​,
  * ​float​ y ​string​.
  * Programe una sobrecarga de la función ​Mayor(...) que
  * reciba un vector y retorne el mayor elemento del mismo.
@@ -12,6 +12,7 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 template <typename T> T mayor(T a, T b) {
@@ -25,33 +26,122 @@ template <typename T> T mayor(T a, T b) {
   return may;
 }
 
-int main() {
-  int valor, intA, intB;
-  float floatA, floatB;
-  string stringA, stringB;
-  cout << "Seleccione que variables desea comparar\n"
-       << "1) Enteros\n"
-       << "2) Flotantes\n"
-       << "3) Strings\n";
-  cin >> valor;
+template <typename U> U mayor(const vector<U> v) {
+  U may = v[0];
+  for (int i = 0; i < v.size(); i++) {
+    if (v[i] > may) {
+      may = v[i];
+    }
+  }
 
-  cout << "Ingrese variables a comparar\n";
-  switch (valor) {
+  return may;
+}
+
+int main() {
+  int rta1, rta2;
+
+  cout << "Seleccione que desea comparar\n"
+       << "1) Variables\n"
+       << "2) Vectores\n";
+  cin >> rta1;
+
+  switch (rta1) {
   case 1:
-    cin >> intA >> intB;
-    cout << "El mayor es: " << mayor(intA, intB);
+    cout << "Seleccione que variables desea comparar\n"
+         << "1) Enteros\n"
+         << "2) Flotantes\n"
+         << "3) Strings\n";
+    cin >> rta2;
 
     break;
 
   case 2:
-    cin >> floatA >> floatB;
-    cout << "El mayor es: " << mayor(floatA, floatB);
+    cout << "Seleccione que tipo de vector desea comparar\n"
+         << "1) De enteros\n"
+         << "2) De flotantes\n"
+         << "3) De strings\n";
+    cin >> rta2;
 
     break;
+  }
 
-  case 3:
-    cin >> stringA >> stringB;
-    cout << "El mayor es: " << mayor(stringA, stringB);
+  if (rta1 == 1) {
+    switch (rta2) {
+    case 1: {
+      cout << "Ingrese variables a comparar\n";
+      int intA, intB;
+      cin >> intA >> intB;
+      cout << "El mayor es: " << mayor(intA, intB);
+
+      break;
+    }
+
+    case 2: {
+      cout << "Ingrese variables a comparar\n";
+      float floatA, floatB;
+      cin >> floatA >> floatB;
+      cout << "El mayor es: " << mayor(floatA, floatB);
+
+      break;
+    }
+
+    case 3: {
+      cout << "Ingrese variables a comparar\n";
+      string stringA, stringB;
+      cin >> stringA >> stringB;
+      cout << "El mayor es: " << mayor(stringA, stringB);
+      break;
+    }
+    }
+  } else if (rta1 == 2) {
+    switch (rta2) {
+    case 1: {
+      cout << "Ingrese variables para cargar el vector y luego compararlas\n";
+      vector<int> v;
+      int var;
+
+      do {
+        cout << "Ingrese el int. (-9999 para terminar)\n";
+        cin >> var;
+        v.push_back(var);
+      } while (var != -9999);
+
+      cout << "El mayor valor es: " << mayor(v);
+
+      break;
+    }
+
+    case 2: {
+      cout << "Ingrese variables para cargar el vector y luego compararlas\n";
+      vector<float> v;
+      float var;
+
+      do {
+        cout << "Ingrese el float. (-9999 para terminar)\n";
+        cin >> var;
+        v.push_back(var);
+      } while (var != -9999);
+
+      cout << "El mayor valor es: " << mayor(v);
+
+      break;
+    }
+
+    case 3: {
+      cout << "Ingrese variables para cargar el vector y luego compararlas\n";
+      vector<string> v;
+      string var;
+
+      do {
+        cout << "Ingrese el string. (AAAA para terminar)\n";
+        cin >> var;
+        v.push_back(var);
+      } while (var != "AAAA");
+
+      cout << "El mayor valor es: " << mayor(v);
+      break;
+    }
+    }
   }
 
   return 0;
