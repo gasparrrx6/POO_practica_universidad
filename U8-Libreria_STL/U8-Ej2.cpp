89c8a3e27a29d85de88df8fd31a832b58af79df1
gasparrrx6 <gasparpelayo44@gmail.com>
2022-11-04 10:59:39 -0300

    Terminado U8-Ej2

--- U8-Libreria_STL/U8-Ej2.cpp
+++ U8-Libreria_STL/U8-Ej2.cpp
@@ -11,14 +11,52 @@
  * utilizando la función remove.
  */
 
+#include <algorithm>
 #include <iostream>
+
 using namespace std;
 
 int main() {
-  int *v = new int(30);
+  int *v = new int[30];
 
   for (int i = 0; i < 30; i++) {
     v[i] = rand() % 26;
+    cout << v[i] << " ";
+  }
+
+  // a
+  sort(v + 10, v + 20);
+  reverse(v, v + 30);
+  int *v2 = new int[10];
+
+  // b
+  copy(v + 10, v + 20, v2); // esto es equivalente al if de abajo
+
+  cout << "\nMuestra el vector ordenado" << endl;
+  for (int i = 0; i < 30; i++) {
+    cout << v[i] << " ";
+    // if (i >= 9 && i <= 19)
+    //   v2[i - 9] = v[i];
+  }
+
+  cout << "\nMuestra el vector reducido" << endl;
+  for (int i = 0; i < 10; i++) {
+    cout << v[i] << " ";
+  }
+
+  // c
+  int nroBuscar;
+  cout << "\nIngrese numero a buscar y eliminar\n";
+  cin >> nroBuscar;
+  auto cantApariciones = count(v, v + 30, nroBuscar);
+  cout << "El nro " << nroBuscar << " aparece " << cantApariciones
+       << " veces\n";
+
+  // d
+  auto itNuevoFinal = remove(v, v + 30, nroBuscar);
+
+  for (auto i = v; i < itNuevoFinal; i++) {
+    cout << v[i - v] << " ";
   }
 
   return 0;
