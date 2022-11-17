b1c7aae8d32d571f98b07ecb13495641177ff3c6
gasparrrx6 <gasparpelayo44@gmail.com>
2022-11-16 21:20:08 -0300

    Un poco de codigo

--- Extras/Ej4/Ej4.cpp
+++ Extras/Ej4/Ej4.cpp
@@ -11,3 +11,64 @@
  * como contraseña para todos los usuarios que tengan alguna de las contraseñas
  * de la lista.
  */
+
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct User {
+  int codigo;
+  char nombre[25], pass[15]; // Definir en exceso
+};
+
+struct Contrasena {
+  string texto; // Contraseña en sí
+  int repet;    // cant de veces repetida
+};
+
+ostream &operator<<(ofstream &o, Contrasena c) {
+  o << "Pass: " << c.texto << " se repite: " << c.repet;
+  return o;
+}
+
+int main() {
+  ifstream datos("datos.txt");
+  fstream archi("usuarios.bin", ios::binary | ios::in);
+  vector<User> lista;
+  int cod;
+  string nomb, pas = 0;
+
+  while (datos >> cod && datos >> nomb && datos >> pas) {
+    User u;
+
+    u.codigo = cod;
+    strcpy(u.nombre, nomb.c_str());
+    strcpy(u.pass, pas.c_str());
+
+    lista.push_back(u);
+  }
+
+  vector<Contrasena> listaPass;
+
+  for (User u : lista) {
+    for (int i = 0; i < listaPass.size(); i++) {
+      if (u.pass != listaPass[i].texto) {
+        Contrasena cs;
+        cs.texto = u.pass;
+        cs.repet += 1;
+        listaPass.push_back(cs);
+      } else if (u.pass == listaPass[i].texto) {
+        listaPass[i].repet++;
+      }
+    }
+
+    for (Contrasena b : listaPass) {
+      cout << b.texto << " " << b.repet << endl;
+    }
+
+    return 0;
+  }
+}
